3199b5552179547c84b155e40175e9352b519f25
cgarcia1196 <cgarcia1196@gmail.com>
2025-08-13 21:23:36 -0700

    added signup page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import "./App.css";
 import ResumeUpload from "./components/ResumeUpload";
 import PortfolioPage from "./pages/portfolios/projectManager/pages/PortfolioPage";
 import ExamplePortfolios from "./components/examplePortfolios";
+import SignUp from "./pages/login/SignUp";
 
 export default function App() {
   const [adminRequested, setAdminRequested] = useState(false);
@@ -31,6 +32,8 @@ export default function App() {
           path="/dashboard"
           element={<Dashboard onRequestAdmin={handleRequestAdmin} />}
         />
+
+        <Route path={"/signup"} element={<SignUp />} />
 
         <Route path="/resume" element={<ResumeUpload />} />
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import { useState } from "react";
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen w-full pt-12">{children}</main>
+      <main className="min-h-screen w-full pt-20">{children}</main>
     </>
   );
 }
