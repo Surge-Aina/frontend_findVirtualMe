112cc372e84ab0f2aa9c396f2dc33f319b44a85a
jacquelanx <109934084+jacquelanx@users.noreply.github.com>
2025-08-13 21:10:50 -0500

    Frontend appears to work... haven't tried running with backend yet but I'll do that tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import "./App.css";
 import ResumeUpload from "./components/ResumeUpload";
 import PortfolioPage from "./pages/portfolios/projectManager/pages/PortfolioPage";
+import PhotographerPage from "./pages/portfolios/photographer/PhotographerApp.jsx"
 import ExamplePortfolios from "./components/examplePortfolios";
 
 export default function App() {
@@ -42,7 +43,7 @@ export default function App() {
         <Route path="/portfolios/data-scientist" />
         <Route path="/portfolios/cleaning-services" />
         <Route path="/portfolios/local-food-vendor" />
-        <Route path="/portfolios/photographer" />
+        <Route path="/portfolios/photographer/*" element={<PhotographerPage/>}/>
         <Route path="/portfolios/handyman" />
       </Routes>
       {adminRequested && (
